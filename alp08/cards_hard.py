f5c85112750a87ddcf7a185fc910d55d9a7c2c5f
koubaj <jx2004.1@gmail.com>
2024-11-22 17:27:57 +0100

    input reading of alp08 hw

--- alp08/cards_hard.py
+++ alp08/cards_hard.py
@@ -1,10 +1,18 @@
 import sys
 
+class Position:
+    def __init__ (self, x, y):
+        self.x = x # row position
+        self.y = y # col position
+        self.color = 0
+        self.value = 0
+        self.empty = True
+
 class Card:
-    def __init__ (self, R, C, arr):
-        self.R = R
-        self.C  = C
-        self.arr = arr
+    def __init__ (self, R, C, colors):
+        self.R = R # rows
+        self.C  = C # collums
+        self.colors = colors
 
 def PM(arr):
     for i in arr:
@@ -15,9 +23,9 @@ def PM(arr):
 filename = sys.argv[1]
 with open(filename) as file:
     r, c = map(int, file.readline().split())
-    m, n = map(int, file.readline().split())
+    m, n = map(int, file.readline().split()) # cards on table, cards in hand
+    arr = [[Position(i, j) for j in range(c)] for i in range(r)]
 
-    arr = [[5] * c for i in range(r)]
     for i in range(m):
         temp = list(map(int, file.readline().split()))
         ri, ci = temp[:2]
@@ -25,17 +33,35 @@ with open(filename) as file:
         colors = temp[4:]
         for i in range(Ri):
             for j in range(Ci):
-                arr[ri + i][ci + j] = colors[i*j + j]
-    cards = []
+                arr[ri + i][ci + j].color = colors[i*Ci + j]
+                arr[ri + i ][ci + j].empty = False
+
+    temp_cards = []
     for i in range(n):
         temp = list(map(int, file.readline().split()))
         Ri, Ci = temp[:2]
         colors = temp[2:]
-        new = Card(Ri, Ci, colors)
-        cards.append(new)
+        temp_cards.append(Card(Ri, Ci, colors))
 
-for card in cards:
-    c1  = [card[i] for i in range()]
-    n1 = Card(card.R, card.C, c1)
-    print(card.arr)
-    print(c1)
+cards = []
+for card in temp_cards:
+    cards.append(card)
+
+    temp_colors = []
+    for i in range(len(card.colors) - 1, -1, -1):
+        temp_colors.append(card.colors[i])
+    cards.append(Card(card.R, card.C, temp_colors))
+
+    temp_colors = []
+    for i in range(card.C):
+        for j in range(card.R - 1, -1, -1):
+            temp_colors.append(card.colors[j * card.C + i])
+    cards.append(Card(card.R, card.C, temp_colors))
+
+    temp_colors = []
+    for i in range(card.C - 1, -1, -1):
+        for j in range(card.R):
+            temp_colors.append(card.colors[j * card.C + i])
+    cards.append(Card(card.R, card.C, temp_colors))
+
+# vstup je přečtený, projít a určit potenciální hodnoty po přidání karty
