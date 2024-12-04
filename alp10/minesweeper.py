cdeae2dfab0d0ef0621f1272cc3611582e7f28a1
koubaj <jx2004.1@gmail.com>
2024-12-04 01:02:18 +0100

    hw 10 completed

--- alp10/minesweeper.py
+++ alp10/minesweeper.py
@@ -2,8 +2,9 @@ import sys
 
 class Place():
     def __init__ (self, num):
-        self.num = num
-        self.mine = False 
+        self.num = num 
+        self.now = -1 
+        self.mine = True 
         self.done = False
         self.neighbours = []
 
@@ -17,6 +18,12 @@ with open(filename) as file:
 r = len(arr)
 c = len(arr[0])
 
+def print_res():
+    for i in arr:
+        for j in i:
+            print("X" if j.mine else ".", end = "")
+        print()
+
 moves = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 for i in range(r):
     for j in range(c):
@@ -25,6 +32,7 @@ for i in range(r):
             np = [i + m[0], j + m[1]]
             if np[0] >= 0 and np[1] >= 0 and np[0] < r and np[1] < c:
                 arr[i][j].neighbours.append([np[0], np[1]])
+        arr[i][j].now = len(arr[i][j].neighbours)
 
 for i in range(r):
     for j in range(c):
@@ -32,19 +40,14 @@ for i in range(r):
             for np in arr[i][j].neighbours:
                 arr[np[0]][np[1]].mine = True
                 arr[np[0]][np[1]].done = True
+
+        if arr[i][j].num == 0 and not arr[np[0]][np[1]].done:
+            for np in arr[i][j].neighbours:
+                arr[np[0]][np[1]].mine = False
+                arr[np[0]][np[1]].done = True
             for np in arr[i][j].neighbours:
                 for nnp in arr[np[0]][np[1]].neighbours:
-                    arr[nnp[0]][nnp[1]].num -= 1
-
-        if arr[i][j].num == 0:
-            for np in arr[i][j].neighbours:
-                arr[np[0]][np[1]].done = True
-
-def print_res():
-    for i in arr:
-        for j in i:
-            print("X" if j.mine else ".", end = "")
-        print()
+                    arr[nnp[0]][nnp[1]].now -= 1
 
 def check_ok(pos):
     for np in arr[pos[0]][pos[1]].neighbours:
@@ -53,7 +56,7 @@ def check_ok(pos):
             score = 0
         if pos[0] == r-1 and (score == 3 or (pos[1] == c-1 and score == 4)):
             score = 0
-        if arr[np[0]][np[1]].num > score or arr[np[0]][np[1]].num < 0:
+        if arr[np[0]][np[1]].now - arr[np[0]][np[1]].num > score or arr[np[0]][np[1]].now < arr[np[0]][np[1]].num:
             return False
     return True
 
@@ -66,15 +69,15 @@ def recursion(arr, pos):
     if check_ok(pos):
         recursion(arr, np)
     if not arr[pos[0]][pos[1]].done:
-        arr[pos[0]][pos[1]].mine = True
+        arr[pos[0]][pos[1]].mine = False
         for npp in arr[pos[0]][pos[1]].neighbours:
-            arr[npp[0]][npp[1]].num -= 1
+            arr[npp[0]][npp[1]].now -= 1
 
         if check_ok(pos):
             recursion(arr, np)
         
         for npp in arr[pos[0]][pos[1]].neighbours:
-            arr[npp[0]][npp[1]].num += 1
-        arr[pos[0]][pos[1]].mine = False
+            arr[npp[0]][npp[1]].now += 1
+        arr[pos[0]][pos[1]].mine = True
 
 recursion(arr, [0, 0])
